cafbd13be43725cad2057f6784f3ce2fd5571a4d
jnlucas <joao.lucas@duosystem.com.br>
2019-02-08 16:04:47 -0200

    criando nossas funcoes em arquivos separados

--- index.php
+++ index.php
@@ -1,65 +1,18 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 ini_set("display_errors",1);
-$variavel = "asas";
-//refinindo variaveis
-$resultado = 80;
 
-//criando funcoes
-function somar($a,$b){
-
-	 $resultado = $a + $b;
-
-	return $resultado;
-}
-
-// chamando funcoes
- somar(12,14);
-
-
-
-// +
-// -
-// *
-// /
-// %
-
-// >
-// <
-// >=
-// <=
-// ==  1==2
-// =
-// ===
-// !=
-// !==
-
-
-
-
- function media($nota ){
- 	if($nota > 5){
- 		$nota = "aprovado";
- 	}elseif($nota === 5){
- 		$nota = "é exatamente a $nota";
- 	}elseif($nota == 5){
- 		$nota = "é somente $nota";
- 	}else{
- 		$nota = "nenhuma das anteriores";
- 	}
-
- 	return $nota;
-
- }
-
-
-
-
-  //media(5);
-
+include_once "funcoes.php";
 
 
 $valorDoFormulario = @$_POST['media'];
+
+
+
+//numerosPrimos(0,50);
+
+
+
 
 
 
@@ -82,6 +35,48 @@ $valorDoFormulario = @$_POST['media'];
 <input type="submit" value="enviar"></input>
 </form>
 <?php echo media($valorDoFormulario);?>
+
+
+
+
+
+
+<!-- exercicio proxima aula
+1234567891011
+
+xooooooooox    1
+oxoooooooxo    2
+ooxoooooxoo    3
+oooxoooxooo    4
+ooooxoxoooo    5
+oooooxooooo    6
+ooooxoxoooo    7
+oooxoooxooo    8
+ooxoooooxoo    9
+oxoooooooxo    10
+xooooooooox    11
+
+
+
+
+-->
+
+<br />
+
+<?php
+
+
+for ($j=0; $j <= 10 ; $j++) { //laco da linha
+	
+	for ($i=0; $i <= 10; $i++) { //laco da coluna
+		definirX($j,$i);
+	}
+	echo "<br>";
+}
+
+
+?>
+
 
 </body>
 </html>
